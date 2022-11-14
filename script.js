6a191924aeeb63f6e71912c284f9b40fc960e0c8
ellon <mordecaimwema11@gmail.com>
2022-11-14 18:22:18 +0300

    added menu toggler

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ let menuOpen = false;
 menuWrapper.addEventListener("click", (e) => {
   if (!menuOpen) {
     menuWrapper.classList.add("open");
+    document.body.style.overflowY = 'hidden';
     menuOpen = true;
   } else {
     menuWrapper.classList.remove("open");
+    document.body.style.overflowY = 'visible';
     menuOpen = false;
   }
 
