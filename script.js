b607c5eaf471f9e24f714db133d793b220fbdfdc
ellon <mordecaimwema11@gmail.com>
2022-11-15 00:42:09 +0300

    added styles for menu and linked each menu item to specific section on page

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-const menuWrapper = document.querySelector(".menu-button-wrap");
-const navLinks = document.querySelector(".nav-links");
+const menuWrapper = document.querySelector('.menu-button-wrap');
+const navLinks = document.querySelector('.nav-links');
+const navLink = document.querySelectorAll('.nav-link');
+const background = document.querySelector('header');
 let menuOpen = false;
 
-menuWrapper.addEventListener("click", (e) => {
+menuWrapper.addEventListener('click', () => {
   if (!menuOpen) {
-    menuWrapper.classList.add("open");
-    document.body.style.overflowY = 'hidden';
+    menuWrapper.classList.add('open');
+    background.style.background = '#e9e9e9';
     menuOpen = true;
   } else {
-    menuWrapper.classList.remove("open");
-    document.body.style.overflowY = 'visible';
+    menuWrapper.classList.remove('open');
+    background.style.background = '#fff';
     menuOpen = false;
   }
 
-  navLinks.classList.toggle("open");
+  navLinks.classList.toggle('open');
+});
+
+navLink.forEach((link) => {
+  link.addEventListener('click', () => {
+    menuWrapper.classList.remove('open');
+    background.style.background = '#fff';
+    menuOpen = false;
+    navLinks.classList.toggle('open');
+  });
 });
