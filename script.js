9ad01cb4963d7cb4162f4869bb68afb4b79aeb76
ellon <mordecaimwema11@gmail.com>
2022-11-15 18:13:10 +0300

    create objects to store project data, implement details popup window

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ const navLink = document.querySelectorAll('.nav-link');
 const background = document.querySelector('header');
 let menuOpen = false;
 
+const openLinks =  document.querySelectorAll('.work-link');
+const popupCards = document.querySelectorAll('.work-details');
+const closeLinks = document.querySelectorAll('.details-close-button');
+
+
 menuWrapper.addEventListener('click', () => {
   if (!menuOpen) {
     menuWrapper.classList.add('open');
-    background.style.background = '#e9e9e9';
     menuOpen = true;
   } else {
     menuWrapper.classList.remove('open');
-    background.style.background = '#fff';
     menuOpen = false;
   }
 
@@ -21,8 +24,38 @@ menuWrapper.addEventListener('click', () => {
 navLink.forEach((link) => {
   link.addEventListener('click', () => {
     menuWrapper.classList.remove('open');
-    background.style.background = '#fff';
     menuOpen = false;
     navLinks.classList.toggle('open');
   });
 });
+
+const setDataIndex = () => {
+  for (i = 0; i < popupCards.length; i++) {
+    openLinks[i].setAttribute('data-index', i);
+    popupCards[i].setAttribute('data-index', i);
+    closeLinks[i].setAttribute('data-index', i);
+  }
+};
+
+// open popup action
+openLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    var index = link.getAttribute('data-index');
+    console.log(index);
+    popupCards[index].classList.add('open');
+    // popupCards[index].style.display = 'flex';
+  });
+});
+
+//close popup action
+closeLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    var index = link.getAttribute('data-index');
+    popupCards[index].classList.remove('open');
+    // popupCards[index].style.display = 'none';
+  });
+});
+
+window.onload = () => {
+  setDataIndex();
+}
